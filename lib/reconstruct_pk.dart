6470fe7456ae460f916de88af12814fdc9c9454e
tonny0831 <tonnyjansen0831@gmail.com>
2023-03-30 18:31:09 +0100

    get shamir secret sharing working server side reconstruction

--- lib/reconstruct_pk.dart
+++ lib/reconstruct_pk.dart
@@ -1,15 +1,32 @@
-import 'dart:js';
-
+import 'get_local_keyshare.dart';
+import 'get_moongate_keyshare.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'secure_storage_util.dart';
-import 'package:ntcdcrypto/ntcdcrypto.dart';
 
-Future<void> reconstructPrivateKey(String token) async {
-  SSS sss = SSS();
+Future<void> reconstructPrivateKey() async {
+  String localKeyShare = await getLocalKeyShare();
+  String moongateKeyShare = await getMoongateKeyShare();
+  print(localKeyShare);
   final secureStorage = SecureStorageUtil();
-  String? keyShare = await secureStorage.getKeyShare();
-  if (keyShare != null) {
-    print('key share is $keyShare');
-  } else {
-    print('key share not found');
+  /*  String? accessToken = await secureStorage.getAccessToken(); */
+/*   if (accessToken != null) { */
+  final response =
+      await http.post(Uri.parse('http://192.168.0.25:3007/keyReconstruction'),
+          headers: {
+            'x-api-key': 'itu9sgo42ig0hhkp5xvfk',
+            'Content-Type': 'application/json',
+          },
+          body: jsonEncode({
+            /*  "access_token": accessToken, */
+            "shareA": localKeyShare,
+            "shareB": moongateKeyShare,
+          }));
+
+  if (response.statusCode == 200) {
+    // If the server returns a 200 OK response, parse the data
+    var data = response.body;
+    print('Reconstructed Private Key: $data');
   }
+  /*  } */
 }
